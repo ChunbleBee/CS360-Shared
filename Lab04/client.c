2777432025d27209ac83f3f61b15e161390bc86b
ChunbleBee <45439546+ChunbleBee@users.noreply.github.com>
2020-03-04 09:59:30 -0800

    Lab04, Client.c - Put all functions from previous file into new Client.c

--- Lab04/client.c
+++ Lab04/client.c
@@ -5,10 +5,20 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <fcntl.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <time.h>
+
 #define LINEMAX 256
 
 struct hostent * host_entry;
 struct sockaddr_in server_addr;
+
+char * permAvailable =  "xwrxwrxwr-------";
+char * permRestricted = "----------------";
 
 struct in_addr server_ip;
 
@@ -41,10 +51,123 @@ void client_init(char * argv[]) {
         exit(4);
     }
     printf("connected to \n");
-    char ip[16];
+    char ip[24];
     inet_ntop(AF_INET, (struct in_addr *) host_entry->h_addr_list[0], ip, sizeof(ip));
     printf("    hostname=%s IP=%s port=%d\n", host_entry->h_name,
             ip, server_port);
+}
+
+// Author: Taiya Williams
+void cat(char * line) {
+    strtok(line, " ");
+    char * file = strtok(NULL, " ");
+
+    if (access(file, F_OK) >= 0) {
+        char buffer[1024];
+        int fdesc = open(file, O_RDONLY);
+
+        if (fdesc != -1) {
+            while (read(fdesc, &buffer, 1024) > 0) {
+                printf("%s", buffer);
+                bzero(buffer, 1024);
+            }
+            printf("\n");
+        } else {
+            printf("Error: could not open file [ %s ] for reading.\n", file);
+        }
+    } else {
+        printf("Error: no such file [ %s ] was found.\n", file);
+    }
+}
+
+// Author: Taiya Williams
+void ls(char * line) {
+    strtok(line, " ");
+    char * paramStr = strtok(NULL, " ");
+
+    if (paramStr != NULL) {
+        struct stat * stats = (struct stat *) malloc(sizeof(struct stat));
+        if (lstat(paramStr, stats) == 0) {
+            printf("Permissions Links Group Owner Size Date Name\n");
+            if (S_ISDIR(stats->st_mode)) {
+                lsDir(paramStr);
+            } else {
+                lsFile(paramStr);
+            }
+        } else {
+            printf("Error: no such file or directory [ %s ] found.\n", paramStr);
+        }
+        free(stats);
+    } else {
+        lsDir("./");
+    }
+}
+
+// Author: Taiya Williams
+void lsDir(char * dirStr) {
+    DIR * dir = opendir(dirStr);
+
+    if (dir != NULL) {
+        struct dirent * treebeard = readdir(dir);
+        // "The world is changing:
+        // I feel it in the water,
+        // I feel it in the earth,
+        // and I smell it in the air."
+        // Treebeard, The Two Towers, J. R. R. Tolkien.
+
+        char path[4356]; //max path length + entry name size = 4096 + 260 = 4356 characters
+        while(treebeard != NULL) {
+            bzero(path, 4356);
+            strcat(&path, dirStr);
+            strcat(&path, treebeard->d_name);
+            lsFile(&path);
+            treebeard = readdir(dir);
+        }
+    } else {
+        printf("Error: no such directory [ %s ] found.\n", dirStr);
+    }
+}
+
+// Author: Taiya Williams
+void lsFile(char * fileStr) {
+    if (access(fileStr, F_OK) == 0) {
+        struct stat * stats = (struct stat *) malloc(sizeof(struct stat));
+        lstat(fileStr, stats);
+
+        char type = '0'; //Other/unknown type
+
+        if (S_ISDIR(stats->st_mode)) {
+            type = 'd';
+        } else if (S_ISREG(stats->st_mode)) {
+            type = '-';
+        } /*else if (S_ISLINK(stats->st_mode)) {
+            type = 'l';
+        }*/
+
+        printf("%c", type);
+        for (int i = 0; i < 8; i++) {
+            if (stats->st_mode & (1 << i)) { // print r | w | x
+                printf("%c", permAvailable[i]);
+            } else {
+                printf("%c", permRestricted[i]);
+            }
+        }
+
+        char * filetime = ctime(&(stats->st_ctime));
+        filetime[strlen(filetime) - 1] = "\0";
+        
+        // Permissions Links Group Owner Size Date Name
+        printf(" %d", stats->st_nlink);
+        printf(" %d", stats->st_gid);
+        printf(" %d", stats->st_uid);
+        printf(" %d", stats->st_size);
+        printf(" %s", filetime);
+        printf(" %s\n", fileStr);
+
+        free(stats);
+    } else {
+        printf("Error: no such file [ %s ] found.\n", fileStr);
+    }
 }
 
 int main (int argc, char * argv[], char * env[]) {
@@ -57,18 +180,81 @@ int main (int argc, char * argv[], char * env[]) {
     }
 
     client_init(argv);
+    while (true) {
+        printf("input a line : ");
+        bzero(line, LINEMAX);                // zero out line[ ]
+        fgets(line, LINEMAX, stdin);         // get a line (end with \n) from stdin
 
-    while (strncmp(line, "quit", 4)) {
-        printf("input a line:\n$ ");
-        bzero(line, LINEMAX);
-        fgets(line, LINEMAX, stdin);
+        line[strlen(line) - 1] = NULL;     // kill \n at end
 
-        line[strlen(line) - 1] = '\0';
-        if (line[0] != '\0') {
+        if (strncmp(line, "quit", 4) == 0){           // exit if NULL line
+            exit(0);
+        } else if (strncmp(line, "lcat", 3) == 0) {
+            cat(line);
+        } else if (strncmp(line, "lpwd", 4) == 0) {
+            char buffer[512];
+            getcwd(&buffer, 512);
+            printf("%s\n", buffer);
+        } else if (strncmp(line, "lls", 3) == 0) {
+            ls(line);
+        } else if (strncmp(line, "lcd", 3) == 0) {
+            //Change local working directory
+        } else if (strncmp(line, "lmkdir", 6) == 0) {
+            strtok(line, " ");
+            char * name = strtok(NULL, " ");
+            char * mode = strtok(NULL, " ");
+
+            if (
+                name != NULL &&
+                opendir(name) == NULL
+            ) {
+                if (mode == NULL) {
+                    mkdir(name, mode);
+                } else {
+                    mkdir(name, "755");
+                }
+            }  else {
+                printf("Error: could not create directory [ %s ].\n", name);
+            }
+        } else if (strncmp(line, "lrmdir", 6) == 0) {
+            strtok(line, " ");
+            char * name = strtok(NULL, " ");
+
+            if (
+                name != NULL &&
+                opendir(name) != NULL
+            ) {
+                rmdir(name);
+            } else {
+                printf("Error: could not remove directory [ %s ].\n", name);
+            }
+        } else if (strncmp(line, "lrm", 3) == 0) {
+            strtok(line, " ");
+            char * name = strtok(NULL, " ");
+
+            if (
+                name != NULL &&
+                access(name, F_OK) == 0
+            ) {
+                remove(name);
+            } else {
+                printf("Error: could not remove file [ %s ].\n", name);
+            }
+        } else {
+            // Send ENTIRE line to server
             n = write(server_socket, line, LINEMAX);
-            printf("client: wrote n=%d bytes:\n    %s\n", n, line);
+            printf("client: wrote n=%d bytes; line=(%s)\n", n, line);
+            
+            // Read a line from sock and show it
             n = read(server_socket, line, LINEMAX);
-            printf("client: read n=%d bytes:\n    %s\n", n, line);
+            printf("client: read  n=%d bytes; echo=(%s)\n",n, line);
+            
+            if (strncmp(n, "get", 3) == 0) {
+
+            } else if (strncmp(n, "put", 3) == 0) {
+
+            }
         }
+
     }
 }
