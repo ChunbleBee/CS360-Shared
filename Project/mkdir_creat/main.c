9d5f5070e5caab3564dcb8926a74598e0e501be6
ChunbleBee <ChunbleBee@github.com>
2020-03-26 14:31:29 -0700

    Pulled global variables out to it's own file.

--- Project/mkdir_creat/main.c
+++ Project/mkdir_creat/main.c
@@ -12,22 +12,7 @@
 #include <stddef.h>
 
 #include "type.h"
-
-// global variables
-MINODE minode[NMINODE];
-MINODE *root;
-
-PROC   proc[NPROC], *running;
-
-char gpath[128]; // global for tokenized components
-char *name[32];  // assume at most 32 components in pathname
-int   n;         // number of component strings
-
-int fd, dev;
-int nblocks, ninodes, bmap, imap, inode_start; // disk parameters
-
-
-/**********************/
+#include "globals.h"
 #include "util.c"
 #include "cd_ls_pwd.c"
 #include "mkdir.c"
